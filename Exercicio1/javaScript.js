1dae9c4771104721895be2fcf16ab943e84bb54d
bigneuzinn <160941317+bigneuzinn@users.noreply.github.com>
2024-07-29 16:21:09 -0300

    Add files via upload

---
+++ Exercicio1/javaScript.js
@@ -0,0 +1,7 @@
+var num1 = parseFloat(prompt("Digite o primeiro numero"));
+var num2 = parseFloat(prompt("Digite o primeiro numero"));
+var resultado = num1 + num2;
+console.log("Soma de dois números:", resultado);
+
+
+
---
+++ Exercicio12/javaScript.js
@@ -0,0 +1,4 @@
+var total = parseFloat(prompt("Digite o primeiro numero"));
+var percentual = parseFloat(prompt("Digite o segundo numero"));
+var resultado = (percentual / 100) * total;
+console.log("Cálculo de percentual:", resultado);
